1195c59157fc8a506a592b84e7ec637d7bed97a1
Matheus Ruggiero <theuscr@gmail.com>
2022-09-23 01:31:25 +1000

    Added offline behaviour

--- rpiedgetier.py
+++ rpiedgetier.py
@@ -1,23 +1,35 @@
+import signal, sys, time
 import serial
-import time
 import string
 import paho.mqtt.publish as publish
 import paho.mqtt.client as mqtt
+import datetime
 
+# Building indentifier -> a raspberry pi corresponds to a building
 thisBuilding = "Building0"
-
-# defines which tty device we should use to communicate
-# via bluetooth
-ttyBluetooth = "/dev/rfcomm0"
 
 # defines MQTT broker IP address
 mqttAddress = "54.193.132.176"
 
-# reading and writing data from and to teensy via serial
-# rfcomm0 corresponds to the bluetooth device
-ser = serial.Serial(ttyBluetooth, 9600)
+# reading and writing data from and to BT serial
+# rfcomm0 corresponds to the bluetooth device representing door01
+ser = serial.Serial("/dev/rfcomm0", 9600)
+serialMap = {"door01":ser}
+
+# structure that temporarily holds credentials in case the cloud
+# tier goes down
+CredentialBuffer = dict()
+
+# maximum time credentials are allowed to be buffered before
+# becoming stale in seconds
+BUFFERING_TIME_MAX = 60*1000
+
+# to keep cloud connection status
+CloudConnected = False
 
 def on_connect(client, userdata, flags, rc): # func for making connection
+	global CloudConnected
+	CloudConnected = True
 	print("Connecting to MQTT")
 	print("Connection returned result: " + str(rc))
 	client.subscribe(thisBuilding + "/emergency")
@@ -27,6 +39,8 @@ def on_message(client, userdata, msg): # func for sending msg
 	payload = msg.payload.decode("utf-8")
 	topicLevels = msg.topic.split('/')
 	numLevels = len(topicLevels)
+	print("### received from MQTT broker: " + msg.topic + " " + payload)
+
 	if nulLevels > 2:
 		building = topicLevels[0]
 		if (building == thisBuilding):
@@ -35,54 +49,108 @@ def on_message(client, userdata, msg): # func for sending msg
 			if level2 == "emergency":
 				emergencyState = "OFF" if level == "0" else "ON"
 				ser.write(str.encode("EMERGENCY," + emergencyState + "\r\n"))
-			elif level2[:4] == "door" and numLevels > 4:
-				doorNumber = level2[4:]
+			elif numLevels > 4:
 				if level3 == "access":
 					tagID = topicLevels[3]
 					permission = "DENIED"
 					if topicLevels[4] == 1:
 						permission = "GRANTED"
 						bufferCredential(tagID, level2)
-					if doorNumber == 1:
-						ser.write(str.encode("ACCESS," + emergencyState + "\r\n"))
-	print("### received from MQTT broker: " + msg.topic + " " + payload)
+					try:
+						serialMap[level2].write(str.encode("ACCESS," + emergencyState + "\r\n"))
+					except:
+						print("Asset not found: " + level2)
 
 def on_publish(client, obj, msg):
 	print("Published to MQTT message id: " + str(msg))
 
-def bufferCredential(tagID, assetID):
-	#TODO buffer credential
-	pass
+def on_disconnect(client, userdata, rc):
+	if rc != 0:
+		global CloudConnected
+		CloudConnected = False
+		print("MQTT disconnected. Attempting to reconnect.")
 
+def bufferCredential(tagID, assetID):
+	global CredentialBuffer
+	GrantTime = datetime.datetime.now()
+	CredentialBuffer[tagID + "," + assetID] = [GrantTime]
+
+def bufferedResponse(tagID, asset):
+	key = tagID + "," + asset
+	access = "DENIED"
+	if key in CredentialBuffer:
+		# Check elapsed time since last access granted online
+		now = datetime.datetime.now()
+		elapsedTime = now - CredentialBuffer[key]
+		if elapsedTime.total_seconds() < BUFFERING_TIME_MAX :
+			access = "GRANTED"
+	else:
+		print("Credential not found: " + tagID + "," + asset)
+
+	if asset in serialMap:
+		serialMap[asset].write(str.encode("ACCESS," + access + "\r\n"))
+	else:
+		print("Could not find asset: " + asset)
+
+def processAllSerials():
+	for asset in serialMap:
+		serial = serialMap[asset]
+		if serial.in_waiting > 0:
+			rawserial = serial.readline()
+			cookedserial = rawserial.decode('utf-8').strip('\r\n')
+			print("Received via bt: " + cookedserial + "\n")
+
+			# it is good practice to avoid jamming the data
+			# bandwidth used in wireless communications of IoT
+			# devices. So I used comma separated values to
+			# encode sensed data collected from sensors. Now
+			# I'll split them back.
+			values = cookedserial.split(",")
+
+			# the message may come broken down and we need to
+			# avoid accessing an element out-of-bounds of the
+			# array.
+			valuesLen = len(values)
+
+			if valuesLen == 2:
+				if values[0] == "TAGSWIPE":
+					TagID = values[1]
+					if CloudConnected :
+						client.publish(thisBuilding +"/" + asset + "/tagswipe", TagID)
+						print("published: " +thisBuilding +"/" + asset + "/tagswipe/" + TagID)
+					else:
+						bufferedResponse(TagID, asset)
+
+
+def signal_handler(sig, frame):
+	print('Exiting…')
+	sys.exit()
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 client.on_publish = on_publish
-client.connect(mqttAddress, 1883, 60)
+client.on_disconnect = on_disconnect
+client.connect(mqttAddress, 1883, 2)
+
+# So we can handle user input
+signal.signal(signal.SIGINT, signal_handler)
+
+# Variable to store last time we attempted reconnection
+SinceReconnect = datetime.datetime.now()
+
+mqttConnInitiated = False
 
 while True:
-	client.loop()
-
-	if ser.in_waiting > 0:
-		rawserial = ser.readline()
-
-		cookedserial = rawserial.decode('utf-8').strip('\r\n')
-		print("Received via bt: " + cookedserial + "\n")
-
-		# it is good practice to avoid jamming the data
-		# bandwidth used in wireless communications of IoT
-		# devices. So I used comma separated values to
-		# encode sensed data collected from sensors. Now
-		# I'll split them back.
-		values = cookedserial.split(",")
-
-		# the message may come broken down and we need to
-		# avoid accessing an element out-of-bounds of the
-		# array.
-		valuesLen = len(values)
-
-		if valuesLen == 2:
-			if values[0] == "TAGSWIPE":
-				TagID = values[1]
-				client.publish(thisBuilding +"/door1/tagswipe", TagID)
+	try:
+		client.loop()
+		if not CloudConnected:
+			now = datetime.datetime.now()
+			elapsed = now - SinceReconnect
+			if elapsed.seconds > 10:
+				SinceReconnect = now
+				client.reconnect()
+				print("Attempting to reconnect…")
+		processAllSerials()
+	except Exception as e:
+		print("An error has occured: ", e)
