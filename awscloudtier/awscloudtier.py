fcda23c2a88b7242fa427bba6e5a09600018b4b5
EC2 Default User <ec2-user@ip-172-31-45-212.ap-southeast-2.compute.internal>
2022-09-25 13:10:09 +0000

    Initial AWS-side release

--- awscloudtier/awscloudtier.py
+++ awscloudtier/awscloudtier.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask_mqtt import Mqtt
 
-airtemp = "airtemp: Not Available"
-
 app = Flask(__name__)
 app.config['MQTT_BROKER_URL'] = 'localhost'  # use the free broker from HIVEMQ
 app.config['MQTT_BROKER_PORT'] = 1883  # default port for non-tls connection
@@ -11,21 +9,83 @@ app.config['MQTT_TLS_ENABLED'] = False  # set TLS to disabled for testing purpos
 
 mqtt = Mqtt(app)
 
+permTable = dict()
+permTable['building0/door01/07003048EC93'] = 1
+#permTable['building0/door01/07003048EC93'] = 1
+#permTable['building0/door01/07003048EC93'] = 1
+#permTable['building0/door01/07003048EC93'] = 1
+#permTable['building0/door01/07003048EC93'] = 1
+
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
 	print("MQTT connected")
-	mqtt.subscribe('airtemp')
+	mqtt.subscribe('#')
 
 @mqtt.on_message()
-def handle_mqtt_message(client, userdata, message):
-	print("MQTT received: " + message.topic + "=" + message.payload.decode())
-	if message.topic == "airtemp":
-		global airtemp
-		airtemp = "airtemp: " + message.payload.decode()
+def handle_mqtt_message(client, userdata, msg):
+	print("MQTT received: " + msg.topic + "=" + msg.payload.decode())
+	topicLevels = msg.topic.split('/')
+	numLevels = len(topicLevels)
+
+	if numLevels == 3:
+		building = topicLevels[0]
+		asset = topicLevels[1]
+		tagswipe = topicLevels[2]
+		tagID = msg.payload.decode()
+		if tagswipe == "tagswipe":
+			access = '0'
+			try:
+				idx = building + '/' + asset + '/' + tagID
+				if permTable[idx] == 1:
+					access = '1'
+			except:
+				print("Not record found in the permissions Table: ", idx)
+			mqtt.publish(building + '/' + asset +'/access/' + tagID, access)
+
+@app.route('/ledon')
+def ledon():
+	mqtt.publish('led','on')
+	return index()
+
+@app.route('/ledoff')
+def ledoff():
+	mqtt.publish('led','off')
+	return index()
+
+@app.route('/buzzer1')
+def buzzer1():
+	mqtt.publish('buzzer','1')
+	return index()
+
+@app.route('/buzzer2')
+def buzzer2():
+	mqtt.publish('buzzer','2')
+	return index()
 
 @app.route('/')
 def index():
-    return airtemp
+	# HTML header tags
+	retHTML  = "<html><head><title>IFN649 Assessment </title>"
+
+	# basic styling
+	retHTML += "<style>body{font-family:verdana;font-size:14px;background-color: #eee;}"
+	retHTML += "a{padding:15px;text-align:center;color:black;margin:5px;border-radius:10px;display:inline-block;text-decoration:none;background-image:linear-gradient(to bottom right, cyan, #c6f6c6);}"
+	retHTML += "div{margin:auto;width:400px;border-radius:10px;background-color:white;padding:10px 30px 20px}</style></head>"
+
+	# Title block
+	retHTML += "<body><div style='margin-top:40px;margin-bottom:10px;background-image:linear-gradient(to bottom right, cyan, #c6f6c6)'><h2>IFN649 Assessment 1</h2></h3>Matheus Cavalca Ruggiero<br />N10913556</h3></div>"
+
+	# update fields according to MQTT messages
+	retHTML += "<div><p>" + airtemp + " &#176C</p>"
+
+	# operate actuators
+	retHTML += "<a href='/ledon'>Turn LED on</a>"
+	retHTML += "<a href='/ledoff'>Turn LED off</a>"
+	retHTML += "<a href='/buzzer1'>Play buzzer track 1</a>"
+	retHTML += "<a href='/buzzer2'>Play buzzer track 2</a>"
+
+	retHTML += "</div></body></html>"
+	return retHTML
 
 if __name__ == '__main__':
 	app.run(debug=True, port=80, host='0.0.0.0')
